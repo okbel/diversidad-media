f2f869ff13519345229307784ce6c568bd95436a
Belen Curcio <curciobelen@gmail.com>
2017-10-25 15:41:57 -0300

    Fixed links and events from ratig

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import s from './Footer.css'
+import {Link} from 'react-router-dom'
 
 class Footer extends React.Component {
   render() {
     return (
       <footer className={s.footer}>
-        Creado con mucho <a className={s.creator}>amor</a>. 
-        <small className={s.message}> Mandame un DM a <a className={s.creator}>@okbel</a> si encontrás algún problema o querés proponer un feature!</small>
+        Creado con mucho <Link className={s.link} to="/sobre">amor</Link>. 
+        <small className={s.message}> Mandame un DM a <a className={s.link} href="https://twitter.com/okbel">@okbel</a> si encontrás algún problema o querés proponer un feature!</small>
       </footer>
     );
   }
