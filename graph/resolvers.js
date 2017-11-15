6dfe13a47fa9636d03141987b5c125a16f7d7f11
lpalmes <lpalmes@gmail.com>
2017-11-15 16:14:36 -0300

    Added shows and movies resolvers

--- graph/resolvers.js
+++ graph/resolvers.js
@@ -1,5 +1,39 @@
-module.exports = {
-  RootQuery: {
-    test: () => 'test',
+const axios = require('axios')
+const credentials = require('../creds.js')
+
+const tmdbRequest = axios.create({
+  baseURL: 'https://api.themoviedb.org',
+  headers: {
+    Authorization: `Bearer ${credentials.tmdb.access_token}`,
+    'Content-Type': `application/json;charset=utf-8`
   }
-};
+})
+
+const ytRequest = axios.create({
+  baseURL: 'https://www.googleapis.com'
+})
+
+module.exports = {
+  Query: {
+    movies: async (root, { page = 1 }) => {
+      try {
+        const { data } = await axios.get(
+          `https://diversidadmedia.com/api/movies?page=${page}`
+        )
+        return data.results
+      } catch (err) {
+        return err
+      }
+    },
+    shows: async (root, { page = 1 }) => {
+      try {
+        const { data } = await axios.get(
+          `https://diversidadmedia.com/api/shows?page=${page}`
+        )
+        return data.results
+      } catch (err) {
+        return err
+      }
+    }
+  }
+}
